0372f6121208b9919e550ae6e6e16d316fa3436d
Phillip Passmore <phillipspassmore@gmail.com>
2022-08-28 20:11:03 +0100

    AB#44 Additional comments explaining code added
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- MicroPython/Touch Sensor example/Two touch sensors LED example.py
+++ MicroPython/Touch Sensor example/Two touch sensors LED example.py
@@ -14,24 +14,31 @@ touchSensor2 = ADC(27)
 active = 65535
 
 while True:
+    #take readings from the touch sensors
     reading1 = touchSensor1.read_u16()
     reading2 = touchSensor2.read_u16()
+    
     time.sleep(0.2)
-    if all ([reading1, reading2] == active):
+    
+    #Checks to see if both sensors are active
+    if reading1 == reading2 == active:
         led.value(1)
         led2.value(1)
         led3.value(1)
         
+    #Otherwise is the right sensor active
     elif reading1 == active:
         led.value(1)
         led2.value(0)
         led3.value(0)
-        
+    
+    #Otherwise is the left sensor active 
     elif reading2 == active:
         led.value(0)
         led2.value(0)
         led3.value(1)
-        
+    
+    #If neither sensors are active the LEDs are set of off. 
     else: 
         led.value(0)
         led2.value(0)
