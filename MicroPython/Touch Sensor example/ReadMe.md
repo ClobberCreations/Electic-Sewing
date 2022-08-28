3994e695b3edd67c8e0ce25cc9ccd18afe518e35
ClobberCreations <clobbercreations@gmail.com>
2022-08-28 14:24:05 +0100

    AB#40 and AB#42 Completed. Simple test code added

---
+++ MicroPython/Touch Sensor example/ReadMe.md
@@ -0,0 +1,8 @@
+# Touch Sensor Example
+
+Welcome to this touch sensor example. More information can be found on my [website](http://www.clobbercreations.co.uk)
+
+## Materials Required:
+
+## Instructions
+
---
+++ MicroPython/Touch Sensor example/main.py
@@ -0,0 +1,24 @@
+from ast import While
+from asyncore import read
+from board import Pin, ADC
+import time
+#Initialising the pins
+led1 = Pin(14, Pin.OUT)
+touchSensor = ADC(27)
+
+# while True for an endless loop
+while True:
+    #takes a reading from ADC pin which will be a number between 0 and 65535
+    reading = touchSensor.readu16() 
+    #prints touchsensor reading 
+    print("ADC : {}".format(reading))
+    #if the touchSensor is pressed down:
+    if reading == 65535:
+        led1.value(1)
+        time.sleep(0.5)
+    #else the LED will be off
+    else:
+        led1.value(0)
+        time.sleep(0.5)
+
+
